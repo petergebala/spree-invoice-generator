d276ceb407c7d5206354f760824945815a673b55
Piotr Gębala <piotrek.gebala@gmail.com>
2012-02-13 17:04:31 +0100

    Button in admin panel for Orders#show

--- app/overrides/add_button_to_admin_panel.rb
+++ app/overrides/add_button_to_admin_panel.rb
@@ -8,3 +8,9 @@ Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
                     :insert_after => "[data-hook='admin_orders_index_row_actions']",
                     :name => "print_button_link",
                     :text => %q{<td><%= link_to "Print", pdf_invoice_prints_path(:order_id => order.id, :format => :pdf) %></td>})
+
+# In Order#show
+Deface::Override.new(:virtual_path => %q{spree/admin/orders/show},
+                    :insert_bottom => "[data-hook='admin_order_show_buttons']",
+                    :name => "print_button_link",
+                    :text => %q{<%= button_link_to "Print invoice", pdf_invoice_prints_path(:order_id => @order.id, :format => :pdf), :icon => 'pdf' %>})
