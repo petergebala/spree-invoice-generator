1c3e66a682d527679bc29e65ba090e3c26cdc582
Thomas von Deyen <tvd@magiclabs.de>
2012-06-06 11:14:37 +0200

    Do not register pdf mimetype. Rails already does this.

--- config/initializers/spree_invoice_generator.rb
+++ config/initializers/spree_invoice_generator.rb
@@ -4,8 +4,6 @@ WickedPdf.config = {
   :exe_path => SpreeInvoiceGenerator::WKHTMLToPDF.bin_path
 }
 
-Mime::Type.register 'application/pdf', :pdf
-
 Spree::InvoicePrint.config = {
   :template_path => File.expand_path(File.dirname(__FILE__)+"/../..") + "/app/views/invoice_prints/invoice_template.html.erb",
   :except_payment => ['Spree::PaymentMethod::Check']
