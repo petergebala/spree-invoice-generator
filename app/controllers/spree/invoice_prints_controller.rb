39bdc1bc21d791883417921653728c0fefdb42aa
Thomas von Deyen <tvd@magiclabs.de>
2012-06-13 15:00:22 +0200

    Create a InvoicePrint record if no one could be found for this order id.
    Fixes an issue where a pdf could not be generated, because the InvoicePrint for that order id was not present.

--- app/controllers/spree/invoice_prints_controller.rb
+++ app/controllers/spree/invoice_prints_controller.rb
@@ -7,16 +7,17 @@ module Spree
     private
     def generate(order)
       order_id = params[:order_id].to_i
-      @invoice_print = current_user.has_role?(:admin) ? InvoicePrint.find_by_order_id(order_id) : current_user.orders.find_by_order_id(order_id)
+      @order = Order.find_by_id(order_id)
+      @invoice_print = current_user.has_role?(:admin) ? InvoicePrint.find_or_create_by_order_id({:order_id => order_id, :user_id => @order ? @order.user_id : nil}) : current_user.invoice_prints.find_or_create_by_order_id(order_id)
       if @invoice_print
         respond_to do |format|
           format.pdf { send_data @invoice_print.generate_pdf, :filename => "invoice_#{@invoice_print.order_id}.pdf", :type => 'application/pdf' }
         end
       else
         if current_user.has_role?(:admin)
-          return redirect_to(admin_orders_path, :notice => "There is no such an order.")
+          return redirect_to(admin_orders_path, :notice => t(:no_such_order_found, :scope => :spree))
         else
-          return redirect_to(orders_path, :alert => "There is no such an order.")
+          return redirect_to(orders_path, :alert => t(:no_such_order_found, :scope => :spree))
         end
       end
     end
