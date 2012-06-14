75e0a9f10ae1818238bb05c5261568b89325fd3c
Thomas von Deyen <tvd@magiclabs.de>
2012-06-14 13:39:02 +0200

    Fixes for popup window settings.

--- app/overrides/add_button_to_admin_panel.rb
+++ app/overrides/add_button_to_admin_panel.rb
@@ -9,7 +9,7 @@ Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
                     :name => "print_button_link",
                     :text => %q{
                       <td>
-                        <%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, pdf_invoice_prints_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=595,height=842,left=100,top=100'); return false;" %>&nbsp;
+                        <%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, pdf_invoice_prints_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %>&nbsp;
                         <%= link_to "#{image_tag('admin/icons/pdf.png')} #{t(:download, :scope => :spree)}".html_safe, pdf_invoice_prints_path(:order_id => order.id, :format => :pdf) %>
                       </td>
                     })
--- app/overrides/add_button_to_customer_panel.rb
+++ app/overrides/add_button_to_customer_panel.rb
@@ -8,6 +8,6 @@ Deface::Override.new(:virtual_path => %q{spree/users/show},
                     :insert_after => "table.order-summary tbody tr td:last",
                     :name => "print_button_link",
                     :text => %q{
-                      <td><%= link_to t(:show, :scope => :spree), pdf_invoice_prints_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=595,height=842,left=100,top=100'); return false;" %></td>
+                      <td><%= link_to t(:show, :scope => :spree), pdf_invoice_prints_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %></td>
                       <td><%= link_to t(:download, :scope => :spree), pdf_invoice_prints_path(:order_id => order.id, :format => :pdf) %></td>
                     })
